2b06e038f7fa19ce107630d789c8c9e46bf02a1e
Uncle Sam <18bn037@esisalama.org>
2020-08-26 18:15:28 +0200

    Scrollreveal.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 //Preloader
 $(document).ready(function(){
 	$('#loader').hide();
-})
+});
+
+//Scroll appear
+// GENERAL SETTING
+window.sr = ScrollReveal();
+sr.reveal('.team', { duration: 2000 });
+sr.reveal('.bar',);
