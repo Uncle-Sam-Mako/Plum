a8367a4197c7f4f7ae8dbd7420eb4880a4e21c59
Uncle Sam <18bn037@esisalama.org>
2020-08-28 13:28:21 +0200

    Adding of fluid anchor

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 //Preloader
 $(document).ready(function(){
 	$('#loader').hide();
+	//ancre-fluide
 });
 
+$('a[href^="#"]').on('click', function(e){
+	e.preventDefault();
+	let link = $(this).attr('href'),
+	elem = $(link);
+	if(elem.length){
+		let elemTop = elem.first().offset().top;
+		$('html, body').animate({
+			scrollTop: elemTop
+		}, "fast");
+	}
+});
 //Scroll appear
 // GENERAL SETTING
